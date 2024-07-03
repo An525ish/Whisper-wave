4ac8aafdbc38ac25ab4b3a7ac5a59c30a5cd36ab
An525ish <an525ish@gmail.com>
2024-07-03 13:38:04 +0530

    added logo and fixed some ui bug

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,7 +3,13 @@ import AppWrapper from "@/layout/AppWrapper"
 const Home = () => {
     return (
         <AppWrapper>
-            Home
+            <div className="grid place-items-center h-full w-full">
+                <div className="text-center">
+                    <img src="/logo-2.jpeg" alt="icon" className="w-[30rem] h-[30rem] rounded-full mix-blend-overlay brightness-125 shadow-xl" />
+                    <p className="capitalize font-[cursive] mt-6 text-3xl font-semibold text-body-300">welcome to whisper wave</p>
+                    <p className="text-xl font-[cursive] font-semibold text-body-300 mt-2">Let's have some fun 👀</p>
+                </div>
+            </div>
         </AppWrapper>
     )
 }
--- client/src/shared/chatListPanel/ChatListHeader.jsx
+++ client/src/shared/chatListPanel/ChatListHeader.jsx
@@ -15,6 +15,7 @@ const ChatHeader = ({ searchText, setSearchText }) => {
         <div className='relative'>
             <div className="w-full flex justify-between items-center p-2">
                 <p className="text-2xl font-semibold text-white flex items-center gap-2">
+                    <img src="/logo-4.png" alt="icon" className="w-12" />
                     Messages
                     {messageNotifications.length > 0 && <span
                         className={`bg-red-dark border border-red-light text-red font-normal text-xs rounded-full ${messageNotifications.length < 9 ? 'w-6' : 'w-fit'} px-1 py-0.5 h-6 text-center grid place-items-center`}>
